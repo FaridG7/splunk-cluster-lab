49efe07e683013d544089e0f1a83ff20de07caad
Farid <goharifarid7@gmail.com>
2026-06-24 17:56:27 +0330

    remove comments

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -2,14 +2,11 @@ output "id" {
   value = libvirt_network.this.id
 }
 output "cidr_address" {
-  # value = "${libvirt_network.this.ips[0].address}/${libvirt_network.this.ips[0].prefix}"
   value = var.cidr_address
 }
 output "gateway" {
-  # value = cidrhost(libvirt_network.this.ips[0].address, 1)
   value = cidrhost(var.cidr_address, 1)
 }
 output "dns" {
-  # value = libvirt_network.this.dns.host[0].ip
   value = cidrhost(var.cidr_address, 1)
 }
