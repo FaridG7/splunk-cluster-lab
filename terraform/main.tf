f923811be2de5cd26a3020b05ef52279f13607d4
Farid <goharifarid7@gmail.com>
2026-06-24 08:03:16 +0330

    add networks

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,19 @@ terraform {
   }
   required_version = "~> 1.15.6"
 }
+
+resource "libvirt_network" "splunk-mgmt-net" {
+  name = "splunk-mgmt-net"
+}
+
+resource "libvirt_network" "splunk-idx-net" {
+  name = "splunk-idx-net"
+}
+
+resource "libvirt_network" "splunk-sh-net" {
+  name = "splunk-sh-net"
+}
+
+resource "libvirt_network" "splunk-lb-net" {
+  name = "splunk-lb-net"
+}
