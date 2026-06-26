faa0cdcd3ef6df1388ca6ab8cc6675cc0f796300
Farid <goharifarid7@gmail.com>
2026-06-26 23:53:29 +0330

    add second play

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,7 +1,10 @@
 resource "libvirt_network" "this" {
   name = "${var.spec.name}-net"
+  forward = {
+    mode = "nat"
+  }
   ips = [{
-    address = cidrhost(var.spec.cidr_address, 0)
+    address = cidrhost(var.spec.cidr_address, 1)
     netmask = cidrnetmask(var.spec.cidr_address)
   }]
 }
